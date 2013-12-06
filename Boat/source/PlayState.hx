5b710a5f754598aafef9d7351677cf7998d767b3
Vinícius <vkdaros@ime.usp.br>
2013-12-06 16:37:21 -0200

    Refactoring: throwing barrels. Throwing barrel on tap also.

--- Boat/source/PlayState.hx
+++ Boat/source/PlayState.hx
@@ -182,14 +182,8 @@ class PlayState extends FlxState {
         if (FlxG.keyboard.pressed("RIGHT")) {
             boat.acceleration.x = BOAT_ACCELERATION;
         }
-        //if (FlxG.keyboard.pressed("SPACE")) {
         if (FlxG.keys.justPressed.SPACE) {
-            if (barrels.countDead() > 0) {
-                var barrel: Sprite = cast(barrels.getFirstDead(), Sprite);
-                barrel.velocity.y = 100;
-                barrel.setPosition(boat.getX(), boat.getY());
-                barrel.revive();
-            }
+            throwBarrel(boat.getX(), boat.getY());
         }
 
         // update barrels
@@ -223,6 +217,9 @@ class PlayState extends FlxState {
                 }
                 else if (touch.x > FlxG.width * (1 - margin)) {
                     boat.acceleration.x = BOAT_ACCELERATION;
+                }
+                else {
+                    throwBarrel(boat.getX(), boat.getY());
                 }
             }
         }
@@ -260,7 +257,14 @@ class PlayState extends FlxState {
 		super.update();
 	}
 
-
+    private function throwBarrel(x: Float, y: Float) {
+        if (barrels.countDead() > 0) {
+            var barrel: Sprite = cast(barrels.getFirstDead(), Sprite);
+            barrel.velocity.y = 100;
+            barrel.setPosition(x, y);
+            barrel.revive();
+        }
+    }
 
     /**
      * Creates a new explosion sprite
